266a8d9c74b15a1b8867dad0f2aa55e00fc9303b
Hanz1log <johanssenabatayo@gmail.com>
2025-05-02 23:52:38 +0800

    Initial full project upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Homescreen from './screens/Homescreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Homescreen />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
