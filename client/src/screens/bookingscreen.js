5ab0ab7127ba2dadf16839e50fb852ff5d01b05a
Hanz1log <johanssenabatayo@gmail.com>
2025-05-06 18:14:35 +0800

    Added a loader and improved ui

--- client/src/screens/bookingscreen.js
+++ client/src/screens/bookingscreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from 'axios';
+import Loader from "../components/Loader";
 
 function Bookingscreen() {
   const { venueid } = useParams();
@@ -14,7 +15,10 @@ function Bookingscreen() {
         setLoading(true);
         const response = await axios.post("/api/venues/getvenuebyid", { venueid });
         setVenue(response.data);
-        setLoading(false);
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
+
       } catch (err) {
         console.error("Booking fetch error:", err.response?.data || err.message);
         setError(true);
@@ -27,37 +31,36 @@ function Bookingscreen() {
 
   return (
     <div className="container mt-5">
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : error ? (
-        <h1>Error...</h1>
-      ) : (
-        <div className="row bs">
-          <div className="col-md-6">
-            <h5>{venue.name}</h5>
-            <img src={venue.imageurls[0]} className="bigimg" alt="Venue" />
-          </div>
-          <div className="col-md-5">
-            <h5>Booking Details</h5>
-            <hr />
-            <p>Name:</p>
-            <p>From Date:</p>
-            <p>To Date:</p>
-            <p>Max Count: {venue.maxcount}</p>
-
-            <h5 className="mt-4">Amount</h5>
-            <hr />
-            <p>Total days:</p>
-            <p>Rent per day: {venue.rentperday}</p>
-            <p>Total Amount:</p>
-            
-            <div style={{float : 'right'}}> 
-              <button className="btn btn-primary mt-3">Pay Now</button>
+      {loading ?
+        (<Loader />) : error ? (
+          <h1>Error...</h1>
+        ) : (
+          <div className="row bs">
+            <div className="col-md-6">
+              <h5>{venue.name}</h5>
+              <img src={venue.imageurls[0]} className="bigimg" alt="Venue" />
             </div>
-           
+            <div className="col-md-5">
+              <h5>Booking Details</h5>
+              <hr />
+              <p>Name:</p>
+              <p>From Date:</p>
+              <p>To Date:</p>
+              <p>Max Count: {venue.maxcount}</p>
+
+              <h5 className="mt-4">Amount</h5>
+              <hr />
+              <p>Total days:</p>
+              <p>Rent per day: {venue.rentperday}</p>
+              <p>Total Amount:</p>
+
+              <div style={{ float: 'right' }}>
+                <button className="btn btn-primary mt-3">Pay Now</button>
+              </div>
+
+            </div>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 }
